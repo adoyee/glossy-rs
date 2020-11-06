efc3b6031e5df713c3b12d72d25d6171611848d0
wangkeen <wangkeen@hzon.com>
2020-11-06 19:23:48 +0800

    saving draft

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,21 +1,41 @@
 use crate::types::Domain;
+use async_std::io;
+use async_std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use async_std::sync::RwLock;
+use futures::future;
 use std::collections::HashMap;
 
 pub enum Protocol {
     HTTP,
     HTTPS,
 }
+
+struct Listener {
+    listener: TcpListener,
+    protocol: Protocol,
+}
+
 pub struct Server {
     domains: RwLock<HashMap<String, Domain>>,
+    listeners: Vec<Listener>,
+    running: bool,
 }
 
 impl Server {
     pub fn new() -> Server {
         let server = Server {
             domains: RwLock::new(HashMap::new()),
+            running: false,
+            listeners: Vec::new(),
         };
         server
+    }
+
+    pub async fn bind<T: ToSocketAddrs>(mut self, addr: T, protocol: Protocol) -> io::Result<()> {
+        let listener = TcpListener::bind(addr).await?;
+        let listener = Listener { listener, protocol };
+        self.listeners.push(listener);
+        Ok(())
     }
 
     pub async fn add_domain(self: &Self, domain: Domain) {
@@ -26,6 +46,21 @@ impl Server {
     pub async fn remove_domain(self: &Self, domain: String) {
         let mut map = self.domains.write().await;
         map.remove(&domain);
+    }
+
+    async fn accept_tcp(self: &Self, listener: &Listener) -> io::Result<(&Listener, TcpStream)> {
+        let result = listener.listener.accept().await;
+        match result {
+            Ok((stream, _)) => Ok((listener, stream)),
+            Err(e) => Err(e),
+        }
+    }
+
+    async fn run(self: &Self) -> io::Result<()> {
+        for &l in self.listeners {
+            let lf = self.accept_tcp(l).await;
+        }
+        Ok(())
     }
 }
 
